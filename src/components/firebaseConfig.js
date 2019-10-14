26f02aa9a7e6a37a632869b09bedca9dccd579b6
Francisco Vizcaino <Panchovizcaino@gmail.com>
2019-10-14 20:32:57 +0200

    returning to project

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-	apiKey: 'AIzaSyDPHa5OKRdFVNUuH3RuaEZOp835EBJv7iM',
-	authDomain: 'two-birds-website.firebaseapp.com',
-	databaseURL: 'https://two-birds-website.firebaseio.com',
-	projectId: 'two-birds-website',
-	storageBucket: 'two-birds-website.appspot.com',
-	messagingSenderId: '948163991014'
-};
